c029f1a6bd4cf009a4b4ef59dc334380ca8ac166
nishi5040 <tejc190080@gmail.com>
2022-06-07 13:45:09 +0900

    add: 引数なしのコンストラクタを自動生成するアノテーションを追加

--- src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
@@ -1,14 +1,14 @@
-package jp.te4a.spring.boot.myapp7;
-
+package jp.te4a.spring.boot.myapp8;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean {
-	Integer id ;
-	String title;
-	String writter;
-	String publisher;
-	Integer price;
+  private Integer id ;
+  private String title;
+  private String writter;
+  private String publisher;
+  private Integer price;
 }
